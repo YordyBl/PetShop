0d3a89ee4237c54e89df5f91112f351d6d94170a
YordyDev <bigsmalldgk@gmail.com>
2024-11-16 10:19:37 -0500

    validation path

--- src/files/files.controller.ts
+++ src/files/files.controller.ts
@@ -1,6 +1,8 @@
 import {
   BadRequestException,
   Controller,
+  Get,
+  Param,
   Post,
   UploadedFile,
   UseInterceptors,
@@ -14,9 +16,15 @@ import { fileNamer } from './helpers/fileNamer';
 @Controller('files')
 export class FilesController {
   constructor(private readonly filesService: FilesService) {}
+  @Get('product/:imageName')
+  findProductImage( @Param('imageName') imageName: string){
+      return this.filesService.getStaticProductImage(imageName);
+  }
 
   @Post('product')
   @UseInterceptors(
+    //El file interceptor me permite accesor a los archivos del body
+    //el file corresponde al con que nombre pasará desde el body
     FileInterceptor('file', {
       fileFilter: fileFilter,
       // limits: {fileSize: 300}
@@ -27,15 +35,17 @@ export class FilesController {
       })
     }),
   ) //el FileInterceptor espera el nombre del body
+  //El UploaedFile sirve para recibir el archivo desde el body
   uploadImage(@UploadedFile() file: Express.Multer.File) {
 
     if(!file){
       throw new BadRequestException('No subiste una imagen')
     }
 
+    const secureUrl = `${file.filename}`;
 
     return {
-      filename: file.originalname,
+      secureUrl
     };
   }
 }
--- src/files/files.service.ts
+++ src/files/files.service.ts
@@ -1,6 +1,19 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { error } from 'console';
+import { existsSync } from 'fs';
+import { join } from 'path';
 
 @Injectable()
 export class FilesService {
+
+    getStaticProductImage( imageName:string){
+        const path = join(__dirname, '../../static/uploads', imageName);
+
+        if( !existsSync(path)){
+            throw new BadRequestException(`No product found with image ${imageName}`)
+        }
+
+        return path
+    }
   
 }
